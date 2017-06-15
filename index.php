685c061f4d69059a98068dc2028bffb84d209f33
yann2084 <y.vernette@gmail.com>
2017-06-15 12:33:05 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+// Required files.
+//require_once(__DIR__ . '/config.php');
+require_once('../../config.php');
+
+// Required and optional parameters.
+$id = required_param('id', PARAM_INT);
+$text = optional_param('text', '', PARAM_ALPHA);
+ 
+// Setting context for the page.
+$PAGE->set_context(context_system::instance());
+// URL is created and then set for the page navigation.
+$url = new moodle_url('/test.php');
+$PAGE->set_url($url);
+// Heading, headers, page layout.
+$PAGE->set_heading(get_string('hello'));
+$PAGE->set_pagelayout('standard');
+echo $OUTPUT->header();
+// Displaying basic content.
+echo html_writer::tag('p', $text);
+// Display the footer.
+echo $OUTPUT->footer();
+
+?>
